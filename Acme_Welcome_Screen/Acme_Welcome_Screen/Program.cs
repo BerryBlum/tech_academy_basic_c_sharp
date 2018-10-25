a7043e4d3158ab800640bdec7a759927a34810b4
BerryBlum <43456325+BerryBlum@users.noreply.github.com>
2018-10-24 18:21:38 -0700

    Welcome to Acme print to console.

---
+++ Acme_Welcome_Screen/Acme_Welcome_Screen/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace Acme_Welcome_Screen
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Acme Accounting Systems");
+            Console.WriteLine("Remember, we're \"accounting\" on you!");
+            Console.Read();
+        }
+    }
+}
---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("What's ypur name?");
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello, " + name + "!");
+        Console.Beep();
+        Console.Read();
+    }
+}
