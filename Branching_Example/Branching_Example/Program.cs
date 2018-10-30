21bc1d09c50e1fa631f2efb3357e4082273f105a
BerryBlum <43456325+BerryBlum@users.noreply.github.com>
2018-10-30 16:14:30 -0700

    Shipping quotation program. Prints to type decimal properly to 2 decimal places. breaks at specified conditions.

---
+++ Branching_Example/Branching_Example/Program.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Branching_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("WHAT IS YOUR FAVORITE NUMBUH??");
+            int fav_num = Convert.ToInt32(Console.ReadLine());
+
+            string result = fav_num == 7 ? "Good person or android depending on when this is being accessed...." : "Lucky numbuh 7 man, should change it to 7";
+
+            Console.WriteLine(result);
+            //int room_temperaure = 70;
+            //Console.WriteLine("WHAT IS YOUR NAME");
+            //string name = Console.ReadLine();
+
+            //Console.WriteLine("Hi," + name + ", what is your current temperature?");
+            //int current_temperature = Convert.ToInt32(Console.ReadLine());
+
+            //if(current_temperature == room_temperaure)
+            //{
+            //    Console.WriteLine("Same temps yo");
+            //}
+            //else if (current_temperature > room_temperaure)
+            //{
+            //    Console.WriteLine("IT HOT OUT FOO");
+            //}
+            //else if (room_temperaure > current_temperature)
+            //{
+            //    Console.WriteLine("IT COLD ISH BRUH");
+            //}
+            //else
+            //{
+            //    Console.WriteLine("ERROR");
+            //}
+            Console.ReadLine();
+            //int current_temperature = 80;
+            //int room_temperature = 70;
+
+            //string compare_results = current_temperature == room_temperature ? "Not the same temps" : "The same temps";
+        }
+    }
+}
---
+++ Shipping_Quote/Shipping_Quote/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shipping_Quote
+{
+    class Program
+    {
+        static void Main()
+        {
+            Console.WriteLine("Welcome to Package Express. Please follow the instruction below.");
+            Console.WriteLine("What is the packages weight?");
+            int package_weight = Convert.ToInt32(Console.ReadLine());
+            if (package_weight > 50)
+            {
+                Console.WriteLine("Package is too heavy to be shipped via Package Express. Have a nice day!");
+                Console.ReadLine();
+                return;
+            }
+            Console.WriteLine("What is the width of the package? In inches:");
+            int package_width = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is the height of the package? In inches:");
+            int package_height = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("What is the length of the package? In inches:");
+            int package_length = Convert.ToInt32(Console.ReadLine());
+            int package_dimensions = package_height + package_length + package_width;
+
+            if (package_dimensions > 50)
+            {
+                Console.WriteLine("Your package is too big too be shipped via Package Express. Have a nice day!");
+                Console.ReadLine();
+                return;
+            }
+            decimal shipping = (((decimal)package_dimensions * (decimal)package_weight) / 100);
+            Console.WriteLine("Your estimated total for shipping is: $" + shipping);
+            Console.WriteLine("Thank you.");
+            Console.ReadLine();
+
+
+        }
+    }
+}
