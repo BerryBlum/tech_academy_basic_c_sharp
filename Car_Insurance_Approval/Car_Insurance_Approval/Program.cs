f8e1ac837269677f05ab3336cd134a7eb300d931
BerryBlum <43456325+BerryBlum@users.noreply.github.com>
2018-10-30 16:33:54 -0700

    Updated dui check variable to type boolean

--- Car_Insurance_Approval/Car_Insurance_Approval/Program.cs
+++ Car_Insurance_Approval/Car_Insurance_Approval/Program.cs
@@ -13,14 +13,13 @@ namespace Car_Insurance_Approval
             Console.WriteLine("What is your age?");
             string user_age = Console.ReadLine();
             int user_age_int = Convert.ToInt16(user_age);
-            Console.WriteLine("Have you ever had a DUI? If so, how many?");
-            string dui_check = Console.ReadLine();
-            int dui_check_int = Convert.ToInt16(dui_check);
+            Console.WriteLine("True or false, you have had a DUI.");
+            bool dui_check = Convert.ToBoolean(Console.ReadLine());
             Console.WriteLine("Have you had any speeding tickets? If so, how many?");
             string speeding_tickets = Console.ReadLine();
             int speeding_ticket_int = Convert.ToInt16(speeding_tickets);
             Console.WriteLine("Are you qualified for car insurance?");
-            if((user_age_int > 15) && (dui_check_int == 0) && (speeding_ticket_int <= 3))
+            if((user_age_int > 15) && (dui_check == false) && (speeding_ticket_int <= 3))
             {
                 Console.WriteLine("true");
             }
