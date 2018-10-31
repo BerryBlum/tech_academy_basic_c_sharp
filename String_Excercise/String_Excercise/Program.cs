03f89bc27c85143f54bb31c328b38cc7c5bf574a
BerryBlum <43456325+BerryBlum@users.noreply.github.com>
2018-10-30 18:29:37 -0700

    Includes string practice and the exercise for creating, concatenating, converting and appending.

---
+++ String_Excercise/String_Excercise/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String_Excercise
+{
+    class Program
+    {
+        static void Main() //Creates a concatenated string, turns it into an uppercase statement and then creates a mini paragraph with it.
+        {
+            string pineapples_0 = "Pineapples ";
+            string pineapples_1 = "are ";
+            string pineapples_2 = "good!";
+            string statement = pineapples_0 + pineapples_1 + pineapples_2;
+            Console.WriteLine(statement);
+            statement = statement.ToUpper();
+            Console.WriteLine(statement);
+
+            StringBuilder string_builder = new StringBuilder();
+
+            string_builder.Append(statement);
+            string_builder.Append(" Coconuts are good!");
+            string_builder.Append(" A pina colada must be the best drink in existence!!!");
+            Console.Write(string_builder);
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Strings_Practice/Strings_Practice/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Strings_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string name = "michael";
+            string quote = "The man said, \" Well, that's not gone well\" \nThis man of course was CLARKSON!";
+            Console.WriteLine(quote);
+            Console.ReadLine();
+
+            bool true_or_false = name.Contains("e");
+            true_or_false = name.EndsWith("e");
+
+            int length = name.Length;
+
+            name = name.ToLower();
+            name = name.ToUpper();
+
+            StringBuilder sb = new StringBuilder();
+
+            sb.Append("My name is Michael");
+
+            Console.WriteLine(sb);
+            Console.ReadLine();
+        }
+    }
+}
