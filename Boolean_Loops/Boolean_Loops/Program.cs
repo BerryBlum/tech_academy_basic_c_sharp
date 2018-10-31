528461adbc9d1fe09387dbf96ea0616fb1628c84
BerryBlum <43456325+BerryBlum@users.noreply.github.com>
2018-10-30 17:36:20 -0700

    Boolean statements for while and do while loops centering around how many bananas are left.

---
+++ Boolean_Loops/Boolean_Loops/Program.cs
@@ -0,0 +1,60 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Boolean_Loops
+{
+    class Program
+    {
+        static void Main() //Boolean comparison for both do while and while loops!
+        {
+            Console.WriteLine("Please tell me how many bananas there are!");
+            int bananas = Convert.ToInt32(Console.ReadLine());
+            
+            while (bananas > 0)
+            {
+                if (bananas == 1)
+                {
+                    Console.WriteLine(bananas + " is left, ONE MOAR TO EAT I BELIEVE IN YOU!!!");
+                    bananas--;
+                }
+                else
+                {
+                    Console.WriteLine(bananas + " are left! Eat moar!!");
+                    bananas--;
+                }
+                
+            }
+            if (bananas == 0)
+            {
+                Console.WriteLine("It's a sad day when there are no bananas.");
+            }
+            
+            Console.ReadLine();
+            Console.WriteLine("Okay, how many bananas were there really?");
+            bananas = Convert.ToInt32(Console.ReadLine());
+            do
+            {
+                switch (bananas)
+                {
+                    case 1:
+                        Console.WriteLine(bananas + " is left...the last banana.");
+                        bananas--;
+                        break;
+                    case 0:
+                        Console.WriteLine("Don't lie about the presence of baanas! If there were none to begin with you should just say that!");
+                        break;
+                    default:
+                        Console.WriteLine(bananas + " are left, you should eat some more. Potassium for days!");
+                        bananas--;
+                        break;
+                }
+            } while (bananas > 0);
+            Console.WriteLine("It's a sad day when there are no bananas.");
+            Console.ReadLine();
+
+        }
+    }
+}
---
+++ Loops_Practice/Loops_Practice/Program.cs
@@ -0,0 +1,76 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Loops_Practice
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Guess a number!!!");
+            int number = Convert.ToInt32(Console.ReadLine());
+            bool is_guessed = number ==7 ;
+
+
+            do
+            {
+                switch (number)
+                {
+                    case 62:
+                        Console.WriteLine("You guessed 63, Try again!");
+                        Console.WriteLine("Guess a number!!!");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 29:
+                        Console.WriteLine("You guessed 29, Try again!");
+                        Console.WriteLine("Guess a number!!!");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 55:
+                        Console.WriteLine("You guessed 55, Try again!");
+                        Console.WriteLine("Guess a number!!!");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                    case 7:
+                        Console.WriteLine("You guessed 7! Good job!");
+                        Console.WriteLine("Guess a number!!!");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        is_guessed = number == 7;
+                        break;
+                    default:
+                        Console.WriteLine("WRONG");
+                        Console.WriteLine("Guess a number!!!");
+                        number = Convert.ToInt32(Console.ReadLine());
+                        break;
+                }
+            } while (!is_guessed);
+
+
+
+
+            //switch(number)
+            //{
+            //    case 62:
+            //        Console.WriteLine("You guessed 63, Try again!");
+            //        break;
+            //    case 29:
+            //        Console.WriteLine("You guessed 29, Try again!");
+            //        break;
+            //    case 55:
+            //        Console.WriteLine("You guessed 55, Try again!");
+            //        break;
+            //    case 7:
+            //        Console.WriteLine("You guessed 7! Good job!");
+            //        is_guessed = true;
+            //        break;
+            //    default:
+            //        Console.WriteLine("WRONG");
+            //        break;
+            //}
+            Console.ReadLine();
+        }
+    }
+}
