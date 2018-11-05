53f6aabeaa9e42d0ee2c53d823fbf165b72dd2f1
BerryBlum <43456325+BerryBlum@users.noreply.github.com>
2018-11-05 15:04:17 -0800

    Updated twenty_one to shuffle multiple times. Included more practice making methods and classes. and different ways to pass numbers into the created method.

---
+++ Class_Method_Practice_113/Class_Method_Practice_113/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Practice_113
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Void_Method.Custom_Method( 5, 6 ); //first instance of custom_method with generic values
+
+            Console.WriteLine("Please give a first integer: ");
+            int var_one = Convert.ToInt32(Console.ReadLine());
+            Console.WriteLine("Please give a second integer: ");
+            int var_two = Convert.ToInt32(Console.ReadLine());
+
+            Void_Method.Custom_Method(var_one, var_two); //passing in two user numbers
+
+            Void_Method.Custom_Method(int_one: var_one, int_two: var_two); //naming those user numbers how they are in the custom method
+
+            Console.ReadLine();
+        }
+    }
+}
---
+++ Class_Method_Practice_113/Class_Method_Practice_113/Void_Method.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Class_Method_Practice_113
+{
+    class Void_Method
+    {
+        public static void Custom_Method(int int_one, int int_two)
+        {
+            int result = int_one * 6;
+            Console.WriteLine(int_one + " is being multiplied by 6 which equals " + result);
+            Console.WriteLine("The second number given is: " + int_two);
+            return;
+        }
+    }
+}
