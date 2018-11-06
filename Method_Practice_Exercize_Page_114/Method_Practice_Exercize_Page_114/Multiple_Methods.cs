791fb2722bccc7f7f9f8f45d07ff2ee5d77a3021
BerryBlum <43456325+BerryBlum@users.noreply.github.com>
2018-11-05 16:06:27 -0800

    Method practice with mispelled title because oops.

---
+++ Method_Practice_Exercize_Page_114/Method_Practice_Exercize_Page_114/Multiple_Methods.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Method_Practice_Exercize_Page_114
+{
+    public class Multiple_Methods
+    {
+        public static int int_return(int user_int) //multiplication
+        {
+            int result = user_int * 4;
+            return result;
+        }
+
+        public static int int_return(double user_int) //division
+        {
+            double result = user_int / 56;
+            return (int) result;
+        }
+
+        public static int int_return(string user_string) //convert to string if possible and take remainder of 100 / user_number , else return 0
+        {
+            try
+            {
+                int result = 100 % Convert.ToInt32(user_string);
+                return result;
+            }
+            catch(FormatException ex)
+            {
+                Console.WriteLine(ex.Message + "Defaulting to return 0");
+                return 0;
+            }
+        }
+    }
+}
---
+++ Method_Practice_Exercize_Page_114/Method_Practice_Exercize_Page_114/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+
+namespace Method_Practice_Exercize_Page_114
+{
+    class Program
+    {
+        static void Main(string[] args) //not 100% certain on the instantiation of the class as it relates to the methods here
+        {
+            Multiple_Methods new_int = new Multiple_Methods(); //this should allow me to call "new_int.int_return()"
+                                                               //because int_return is a method of Multiple_Methods right?
+            int results = Multiple_Methods.int_return(5);
+            Console.WriteLine(results);
+
+            results = Multiple_Methods.int_return(900.432);
+            Console.WriteLine(results);
+
+            results = Multiple_Methods.int_return("12");
+            Console.WriteLine(results);
+
+            Console.ReadLine();
+        }
+    }
+}
