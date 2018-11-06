5228b262465ef6a0210ab5f12c87121ea67cc87d
BerryBlum <43456325+BerryBlum@users.noreply.github.com>
2018-11-05 17:02:54 -0800

    Methods and classes page 115 contains checks for exceptions where there may be no first value or the second value is not capable of being converted to an int or is 0 which would cause a divide by zero exception.

---
+++ Classes_and_Methods_115/Classes_and_Methods_115/Method.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Classes_and_Methods_115
+{
+    class Method
+    {
+        public static int User_Method(int value_one, int value_two = 5) //division computation using values from the main method with a second value defaulting to 5
+        {
+            int results = value_one / value_two;
+            return results;
+        }
+    }
+}
---
+++ Classes_and_Methods_115/Classes_and_Methods_115/Program.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+/// <summary>
+/// instantiates a method from a different class and then asks for two inputs
+/// converts the input strings to integers during the method calls
+/// utilizes try catch for exception handling of code breaking issues
+/// such as null value for first input or string / zero value for second value
+/// </summary>
+namespace Classes_and_Methods_115
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int results = 0;
+            Method method = new Method();
+            Console.WriteLine("Please enter two integers one at a time, a second integer is not necessary, default value is 5.");
+            try
+            {
+                string user_int_one = Console.ReadLine();
+                string user_int_two = Console.ReadLine();
+                if (user_int_two == string.Empty) //checks for empty second value and if true uses first call with only one input
+                {
+                    results = Method.User_Method(Convert.ToInt32(user_int_one));
+                }
+                else // if not blank pushes both values to called method unless aforementioned exceptions break it
+                {
+                    results = Method.User_Method(Convert.ToInt32(user_int_one), value_two: Convert.ToInt32(user_int_two));
+                }
+            }
+            catch (FormatException ex)
+            {
+                Console.WriteLine(ex.Message + " Program ending");
+                Console.ReadLine();
+                return;
+            }
+            catch (DivideByZeroException ex)
+            {
+                Console.WriteLine(ex.Message + " Program ending");
+                Console.ReadLine();
+                return;
+            }
+            Console.WriteLine("The value of the first number divided by the second number is " + results);
+            Console.ReadLine();
+        }
+    }
+}
