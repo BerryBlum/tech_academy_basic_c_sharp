40efff2d3872e523509a2e5b001552400a380cbd
BerryBlum <43456325+BerryBlum@users.noreply.github.com>
2018-11-09 15:01:51 -0800

    Generic type exercise. Must remember to instantiate all parts of the method before they can be accessed.

---
+++ Generics_Exercise/Generics_Exercise/Employee.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Exercise
+{
+    public class Employee<T> //public class of employee
+    {
+        public List<T> Things_Parent { get; set; } //property of Employee to have a list of generic type
+
+        public Employee() //Instantiates Things_Parents as a new List to acept assignment from main method
+        {
+            Things_Parent = new List<T>();
+        }
+    }
+}
+
+
---
+++ Generics_Exercise/Generics_Exercise/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Generics_Exercise
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            Employee<string> Things_String = new Employee<string>(); //instantiate a new item of name Things_String with property of list Things_Parent
+            Employee<int> Things_Int = new Employee<int>(); //Instantiates a new item of name Things_Int with property of list from Things_Parent
+            Things_String.Things_Parent.Add("Banana"); //adds things to both lists of proper types.
+            Things_Int.Things_Parent.Add(42);
+            Things_String.Things_Parent.Add("Banana");
+            Things_Int.Things_Parent.Add(42);
+            Things_String.Things_Parent.Add("Banana");
+            Things_Int.Things_Parent.Add(42);
+            Things_String.Things_Parent.Add("Banana");
+            Things_Int.Things_Parent.Add(42);
+            Things_String.Things_Parent.Add("Banana");
+            Things_String.Things_Parent.Add("Banana");
+            Things_Int.Things_Parent.Add(42);
+            Things_Int.Things_Parent.Add(42);
+
+            foreach(string Items in Things_String.Things_Parent) //loops through each list and prints out all values within
+            {
+                Console.WriteLine(Items);
+            }
+            foreach(int Items in Things_Int.Things_Parent) //foreach is blasphemous!!! easier but blasphemous
+            {
+                Console.WriteLine(Items);
+            }
+            Console.ReadLine();
+        }
+    }
+}
